b958d55b563a39e99df895379a827ae026db2066
Brendan Soffientini <brsoff@gmail.com>
2018-08-15 15:06:57 -0400

    Initial GH pages

---
+++ _downloads/shift_tutorial.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Test Shift Tutorial
+====================================
+**Author**: `Shift`_
+
+Lorem ipsum
+
+To run this tutorial, please make sure the following packages are
+installed:
+"""
+
+print("Hello this is shift lab.")
