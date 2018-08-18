e69688bf1d15ace688c43cad729241f83b06e085
Brendan Soffientini <brsoff@gmail.com>
2018-08-18 12:00:20 -0400

    Add local TOC on right side menu

--- pytorch_sphinx_theme/static/js/theme.js
+++ pytorch_sphinx_theme/static/js/theme.js
@@ -1,5 +1,5 @@
 /* pytorch_sphinx_theme version 0.0.1 | MIT license */
-/* Built 20180815 14:08 */
+/* Built 20180818 11:08 */
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"pytorch-sphinx-theme":[function(require,module,exports){
 var jQuery = (typeof(window) != 'undefined') ? window.jQuery : require('jquery');
 
