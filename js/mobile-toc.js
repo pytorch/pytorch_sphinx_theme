43d6d875be202e019c5f86a1770a8d3d0d891b29
Brendan <brsoff@gmail.com>
2018-09-09 12:00:27 -0400

    [PYT-217] Update mobile menu design (#47)

--- js/mobile-toc.js
+++ js/mobile-toc.js
@@ -1,30 +1,19 @@
 window.mobileTOC = {
   bind: function() {
-    $("[data-behavior='open-table-of-contents']").on("click", function(e) {
+    $("[data-behavior='toggle-table-of-contents']").on("click", function(e) {
       e.preventDefault();
-      $("body").addClass("no-scroll");
-      $(".pytorch-left-menu").addClass("open-mobile");
 
-      mobileTOC.listenForResize();
-    });
+      var $parent = $(this).parent();
 
-    $("[data-behavior='close-table-of-contents']").on("click", function(e) {
-      e.preventDefault();
-      mobileTOC.close();
-    });
-  },
-
-  listenForResize: function() {
-    $(window).on('resize.ForMobileTOC', function() {
-      if ($(this).width() > 768) {
-        mobileTOC.close();
+      if ($parent.hasClass("is-open")) {
+        $parent.removeClass("is-open");
+        $(".pytorch-left-menu").slideUp(200, function() {
+          $(this).css({display: ""});
+        });
+      } else {
+        $parent.addClass("is-open");
+        $(".pytorch-left-menu").slideDown(200);
       }
     });
-  },
-
-  close: function() {
-    $(".pytorch-left-menu").removeClass("open-mobile");
-    $("body").removeClass('no-scroll');
-    $(window).off('resize.ForMobileTOC');
   }
 }
