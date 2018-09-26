6610cf3f6a09cd606c358d7a6e0edd45c6eae89a
Brendan Soffientini <brsoff@gmail.com>
2018-09-25 23:02:06 -0400

    Fix various build errors - Conditionally load Mathjax scripts. It's no longer an included lib in the PyTorch sphinx docs as per https://github.com/pytorch/pytorch/commit/6e85112f12cce9731ae09237769f15f675f3da7b#diff-1ec61223727a10e5e4c74c68336809fcL50 - Remove build sphinx step in 'grunt build' task. We don't need to build a demo app on grunt build. The build step in this case just readies the theme for packaging. - Reflect recent italic font updates in sass file to ensure they are included in new builds.

--- js/mathjax.js
+++ js/mathjax.js
@@ -1,13 +1,15 @@
-MathJax.Hub.Config({
-    messageStyle: "none",
-    scale: 100,
-    "HTML-CSS": {
-        showMathMenu: false,
-        linebreaks: { automatic: true, width: "container" } ,
-        preferredFont: "STIX",
-        availableFonts: ["STIX","TeX"]
-    },
-    SVG: { linebreaks: { automatic: true, width: "container" } }
- });
+if (typeof window.MathJax === "object") {
+  window.MathJax.Hub.Config({
+      messageStyle: "none",
+      scale: 100,
+      "HTML-CSS": {
+          showMathMenu: false,
+          linebreaks: { automatic: true, width: "container" } ,
+          preferredFont: "STIX",
+          availableFonts: ["STIX","TeX"]
+      },
+      SVG: { linebreaks: { automatic: true, width: "container" } }
+   });
 
- MathJax.Hub.Configured();
+  window.MathJax.Hub.Configured();
+}
