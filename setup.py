343874f16ebdbdac337c50d15fed32f28a3c4f9b
Paul Nation <nonhermitian@gmail.com>
2020-11-04 12:04:32 -0500

    switch to different install

--- setup.py
+++ setup.py
@@ -1,31 +1,29 @@
 from setuptools import setup
 from io import open
-from pytorch_sphinx_theme import __version__
+from qiskit_sphinx_theme import __version__
 
 setup(
-    name = 'pytorch_sphinx_theme',
+    name = 'qiskit_sphinx_theme',
     version =__version__,
     author = 'Shift Lab',
     author_email= 'info@shiftlabny.com',
-    url="https://github.com/pytorch/pytorch_sphinx_theme",
-    docs_url="https://github.com/pytorch/pytorch_sphinx_theme",
-    description='PyTorch Sphinx Theme',
-    py_modules = ['pytorch_sphinx_theme'],
-    packages = ['pytorch_sphinx_theme'],
+    url="https://github.com/nonhermitian/qiskit_sphinx_theme",
+    description='qiskit Sphinx Theme',
+    py_modules = ['qiskit_sphinx_theme'],
+    packages = ['qiskit_sphinx_theme'],
     include_package_data=True,
     zip_safe=False,
-    package_data={'pytorch_sphinx_theme': [
+    package_data={'qiskit_sphinx_theme': [
         'theme.conf',
         '*.html',
         'static/css/*.css',
         'static/js/*.js',
-        'static/fonts/*.*',
         'static/images/*.*',
         'theme_variables.jinja'
     ]},
     entry_points = {
         'sphinx.html_themes': [
-            'pytorch_sphinx_theme = pytorch_sphinx_theme',
+            'qiskit_sphinx_theme = qiskit_sphinx_theme',
         ]
     },
     license= 'MIT License',
