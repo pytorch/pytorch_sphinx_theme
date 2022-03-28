6986429bc57fd06a9f5e810ae78c9660d4b07d00
William Falcon <waf2107@columbia.edu>
2022-03-27 22:14:30 -0400

    updated styles

--- pt_lightning_sphinx_theme/extensions/pytorch_tutorials.py
+++ pt_lightning_sphinx_theme/extensions/pytorch_tutorials.py
@@ -131,6 +131,8 @@ class CustomCalloutItemDirective(Directive):
         "description": directives.unchanged,
         "button_link": directives.unchanged,
         "button_text": directives.unchanged,
+        "col_css": directives.unchanged,
+        "card_style": directives.unchanged,
     }
 
     def run(self):
@@ -154,6 +156,16 @@ class CustomCalloutItemDirective(Directive):
                 button_text = self.options["button_text"]
             else:
                 button_text = ""
+            
+            if "col_css" in self.options:
+                col_css = self.options["col_css"]
+            else:
+                col_css = "col-md-6"
+            
+            if "card_style" in self.options:
+                card_style = self.options["card_style"]
+            else:
+                card_style = "text-container"
 
         except FileNotFoundError as e:
             print(e)
@@ -164,7 +176,7 @@ class CustomCalloutItemDirective(Directive):
             return []
 
         callout_rst = CALLOUT_TEMPLATE.format(
-            description=description, header=header, button_link=button_link, button_text=button_text
+            description=description, header=header, button_link=button_link, button_text=button_text, col_css=col_css, card_style=card_style
         )
         callout_list = StringList(callout_rst.split("\n"))
         callout = nodes.paragraph()
@@ -175,9 +187,9 @@ class CustomCalloutItemDirective(Directive):
 CALLOUT_TEMPLATE = """
 .. raw:: html
 
-    <div class="col-md-6">
+    <div class="{col_css}">
         <a href="{button_link}">
-            <div class="text-container">
+            <div class="{card_style}">
                     <h3>{header}</h3>
                     <p class="body-paragraph">{description}</p>
             </div>
