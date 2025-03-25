8958845ef7b14a4088e45569de0a3d71ce52f367
Svetlana Karslioglu <svekars@meta.com>
2025-03-25 14:30:41 -0700

    Update

--- pytorch_sphinx_theme2/static/js/tutorial-buttons-call-to-action.js
+++ pytorch_sphinx_theme2/static/js/tutorial-buttons-call-to-action.js
@@ -11,9 +11,16 @@ if (downloadNote.length >= 1) {
         notebookDownloadPath = notebookLink.split('_downloads')[1],
         colabLink = "https://colab.research.google.com/github/pytorch/tutorials/blob/gh-pages/_downloads" + notebookDownloadPath;
 
-    $("#google-colab-link").wrap("<a href=" + colabLink + " data-behavior='call-to-action-event' data-response='Run in Google Colab' target='_blank'/>");
-    $("#download-notebook-link").wrap("<a href=" + notebookLink + " data-behavior='call-to-action-event' data-response='Download Notebook'/>");
-    $("#github-view-link").wrap("<a href=" + githubLink + " data-behavior='call-to-action-event' data-response='View on Github' target='_blank'/>");
+    // Only add the links if they don't already exist
+    if ($("#google-colab-link").parent().prop("tagName") !== "A") {
+        $("#google-colab-link").wrap("<a href=" + colabLink + " data-behavior='call-to-action-event' data-response='Run in Google Colab' target='_blank'/>");
+        $("#download-notebook-link").wrap("<a href=" + notebookLink + " data-behavior='call-to-action-event' data-response='Download Notebook'/>");
+        $("#github-view-link").wrap("<a href=" + githubLink + " data-behavior='call-to-action-event' data-response='View on Github' target='_blank'/>");
+    }
+
+    // Show the buttons
+    $(".pytorch-call-to-action-links").show();
 } else {
+    // Hide the buttons if not a sphinx-gallery page
     $(".pytorch-call-to-action-links").hide();
 }
