26644995aa98f6650fb6935024c05d631a9bddfa
Svetlana Karslioglu <svekars@meta.com>
2025-04-01 14:14:41 -0700

    Update

--- pytorch_sphinx_theme2/static/js/theme.js
+++ pytorch_sphinx_theme2/static/js/theme.js
@@ -187,19 +187,9 @@ function openGitHubIssue() {
                     window.dataLayer.push({
                         'event': 'star_rating',
                         'Rating': value,
-                        'value': value,
                         'page_path': pagePath,
                         'event_name': 'click',
                         'event_category': 'Page Rating'
-                    });
-                }
-
-                // Direct GA4 event
-                if (typeof gtag == 'function') {
-                    gtag('event', 'star_rating', {
-                        'rating': value,
-                        'page_path': pagePath,
-                        'event_category': 'Page Rating',
                     });
                 }
 
