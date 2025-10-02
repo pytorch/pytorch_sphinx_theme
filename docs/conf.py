1d7d6518eb057766f89e9eff90f9a5ff0f4ca7e7
Svetlana Karslioglu <svekars@meta.com>
2025-10-02 10:21:03 -0700

    Update sphinx footer for non-LF projects

--- docs/conf.py
+++ docs/conf.py
@@ -225,6 +225,8 @@ html_theme_options = {
     # "canonical_url": "https://pytorch.org/docs/stable/",
     "canonical_url": "http://localhost:8000",
     #"pytorch_project": "tutorials",
+    #"show_lf_header": False,
+    #"show_lf_footer": False,
   }
 
 
