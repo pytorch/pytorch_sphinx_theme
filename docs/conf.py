9caed8db8df0d44d2e16c9b22380fa0125c2c814
sekyonda <127536312+sekyondaMeta@users.noreply.github.com>
2026-03-05 12:14:20 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -282,7 +282,7 @@ html_theme_options = {
     "announcement_banner_enabled": True,
     "announcement_banner_text": "Help us understand how you use PyTorch! Take our quick survey.",
     "announcement_banner_url": "https://docs.google.com/forms/d/e/1FAIpQLSfsGAWBcfutRcbO6kfrShBMOMmRuBezRjjOcXk0e9I9luBzvQ/viewform",
-    "announcement_banner_link_text": "Take Survey",
+    "announcement_banner_link_text": "Take Survey.",
 }
 
 
